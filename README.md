86bf0855f122a9a95efee57ccf2202a90e4bd642
Hugo Yanez <hugodavidyanez@gmail.com>
2021-10-25 21:41:26 -0400

    Updated all the files with finished product

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # SERVER APIs: Weather Dashboard
+
+Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+
+Use the OpenWeather One Call API to retrieve weather data for cities. Read through the documentation for setup and usage instructions. You will use localStorage to store any persistent data. For more information on how to work with the OpenWeather API, refer to the Full-Stack Blog on how to use API keys.
+
+## Preview
+
+The following image shows the web applications appearance and functionality:
+
+![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Atlanta.](./assets/weather-dashboard-mockup.png)
+
+### Links
+
+- [Deployment Link](https://hyanez.github.io/Weather-Dashboard/ "Deployment Link")
+- [Github Link](https://github.com/hyanez/Weather-Dashboard "Github Link")
--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,196 @@
-//DEPOSITORIES
+// DEPENDENCIES
+var formEl = $("#form");
+//var formEl = document.querySelector("#form");
+var inputEl = $("#cityInput");
 
-//GLOBAL VARIABLES
+var tempTd = $(".temp").children().eq(1);
+var windTd = $(".wind").children().eq(1);
+var humidityTd = $(".humidity").children().eq(1);
+var UVTd = $(".UV").children().eq(1);
+var tempWeekEl = $("#weekTemp");
+var prevCity = $("prevCity");
+var asideEl = $(".aside");
+var btnContainerEl = $(".btnList");
+var cityEl = $(".cityDate");
 
-//FUNCTIONS
+// GLOBAL VARIABLES
 
-//USER INTERACTIONS
+var hisArr = [];
 
-//INITIALIZATIONS
+// FUNCTIONS
+
+$(function () {
+  var cityChoices = [
+    "Austin",
+    "Chicago",
+    "New York",
+    "Orlando",
+    "San Francisco",
+    "Seattle",
+    "Denver",
+    "Atlanta",
+    "San Diego",
+  ];
+  $("#cityInput").autocomplete({
+    source: cityChoices,
+  });
+});
+
+function onSubmit(event) {
+  event.preventDefault();
+  console.log(inputEl);
+  if (inputEl.val() !== "") {
+    var city = inputEl.val();
+    inputEl.val("");
+    saveSearch(city);
+    renderHistory();
+    renderTodayWeather(city);
+    render5DayWeather(city);
+  }
+}
+
+function saveSearch(city) {
+  var historyArr = JSON.parse(localStorage.getItem("History"));
+  if (historyArr !== null) {
+    historyArr.unshift(city);
+    var strHistory = JSON.stringify(historyArr);
+    localStorage.setItem("History", strHistory);
+  } else {
+    var newArr = [city];
+    var strHistory = JSON.stringify(newArr);
+    localStorage.setItem("History", strHistory);
+  }
+}
+
+function renderHistory() {
+  var tempHistory = JSON.parse(localStorage.getItem("History"));
+  btnContainerEl.empty();
+  if (tempHistory) {
+    for (var i = 0; i < tempHistory.length; i++) {
+      var historyBtn = $("<button>");
+      historyBtn.attr("class", "button");
+      historyBtn.text(tempHistory[i]);
+      historyBtn.attr("data-city", tempHistory[i]);
+      btnContainerEl.append(historyBtn);
+    }
+  }
+}
+
+function showPrevCity() {
+  return prevCity.text(localStorage.getItem("City"));
+}
+
+function renderTodayWeather(city) {
+  GeoLocator(city);
+}
+
+async function GeoLocator(city) {
+  var geoURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=aa772c06902f60c4e5f5e833c0ce31f4`;
+  await fetch(geoURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var lata = data.coord.lat;
+      var long = data.coord.lon;
+      OneCall(city, lata, long);
+    });
+}
+
+function OneCall(city, lata, long) {
+  var todayURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lata}&lon=${long}&units=imperial&appid=d82badb906f2ae8891cd46df1588137f`;
+  fetch(todayURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var today = moment();
+      cityEl.text(`${city}: ${today.format("MMM Do, YYYY")}`);
+      tempTd.text(`${data.current.temp.toFixed(2)}°F`);
+      windTd.text(`${data.current.wind_speed} MPH`);
+      humidityTd.text(`${data.current.humidity}%`);
+      var UVIndex = data.current.uvi;
+      UVTd.text(`${UVIndex}`);
+      //Favorable
+      if (UVIndex <= 2) {
+        UVTd.css("color", "green");
+      }
+      //Moderate
+      else if (UVIndex <= 5) {
+        UVTd.css("color", "orange");
+      }
+      //Severe
+      else {
+        UVTd.css("color", "red");
+      }
+    });
+}
+
+function render5DayWeather(city) {
+  var weekURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=d82badb906f2ae8891cd46df1588137f`;
+  fetch(weekURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var today = moment();
+      var days = tempWeekEl.children().children().children();
+      for (var i = 0; i < days.length; i++) {
+        days
+          .eq(i)
+          .children()
+          .eq(0)
+          .text(`${today.add(1, "days").format("MMM Do, YYYY")}`);
+        days
+          .eq(i)
+          .children()
+          .eq(2)
+          .children()
+          .eq(0)
+          .text(`Temp: ${data.list[i].main.temp}°F`);
+        days
+          .eq(i)
+          .children()
+          .eq(2)
+          .children()
+          .eq(1)
+          .text(`Wind: ${data.list[i].wind.speed} MPH`);
+        days
+          .eq(i)
+          .children()
+          .eq(2)
+          .children()
+          .eq(2)
+          .text(`Humidity: ${data.list[i].main.humidity}%`);
+      }
+    });
+}
+
+// USER INTERACTIONS
+
+formEl.on("submit", onSubmit);
+
+btnContainerEl.on("click", ".button", function (event) {
+  const hisCity = $(event.target).attr("data-city");
+  renderTodayWeather(hisCity);
+  render5DayWeather(hisCity);
+});
+
+// INITIALIZATION
+
+function onRefresh() {
+  var previousSearches = JSON.parse(localStorage.getItem("History"));
+  if (previousSearches !== null) {
+    var previousSearch = previousSearches[0];
+    renderTodayWeather(previousSearch);
+    render5DayWeather(previousSearch);
+    renderHistory();
+  } else {
+    renderTodayWeather("New York");
+    render5DayWeather("New York");
+    renderHistory();
+  }
+  return;
+}
+
+onRefresh();
